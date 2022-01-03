d16441e0eb95f176c14a7ef86e4dc8b18d4e38c7
Nibbles <me@nibbledbit.co.uk>
2022-01-03 20:10:50 +0000

    deploy advanced

--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -1,4 +1,4 @@
-from brownie import SimpleCollectible, network
+from brownie import SimpleCollectible, AdvancedCollectible, network
 from scripts.helpful_scripts import get_account
 
 sample_token_uri = "https://ipfs.io/ipfs/QmVYX1BgugoHzNFsprZHanEYkhWUopUtHmrHsAmTCReABG?filename=nibbles.1.2.json"
@@ -39,5 +39,40 @@ def deploy_simple_collectible():
     return simple_collectible
 
 
+def deploy_advanced_collectible():
+    # Get Account
+    print("Getting account")
+    account = get_account()
+    print(f"Account {account}")
+
+    # configure dependencies
+    print(f"The active network is {network.show_active()}")
+    # if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    #     price_feed_address = config["networks"][network.show_active()][
+    #         "eth_usd_price_feed"
+    #     ]
+    # else:
+    #     deploy_mocks()
+    #     price_feed_address = MockV3Aggregator[-1].address
+    # deploying contracts
+    if len(AdvancedCollectible) <= 0:
+        print("Deploying AdvancedCollectible")
+        advanced_collectible = AdvancedCollectible.deploy({"from": account})
+    else:
+        advanced_collectible = AdvancedCollectible[-1]
+    print(f"Contract deployed to {advanced_collectible}")
+    print(f"Token Counter: {advanced_collectible.tokenCounter()}")
+
+    tx = advanced_collectible.createCollectible({"from": account})
+    tx.wait(1)
+
+    print(
+        f"NFT will be viewable at: {OPENSEA_URL.format(advanced_collectible.address, advanced_collectible.tokenCounter()-1)}"
+    )
+
+    return advanced_collectible
+
+
 def main():
     deploy_simple_collectible()
+    deploy_advanced_collectible()
